913f281ac1915a28a564495c5b60306fa70ca34a
Kjones <kjones404@outlook.com>
2018-04-16 18:16:29 -0500

    Feat: add sidebar data

--- js/app.js
+++ js/app.js
@@ -1,61 +1,72 @@
+// global var
 var map;
+
 
 // load map
 function initMap() {
 
-    // map presets
-    var mapOptions = {
-      zoom: 11,
-      center: {lat: 29.4241226, lng: -98.493629}
-    }
-    // make map
-    map = new
-    google.maps.Map(document.getElementById("map"), mapOptions);
-
-      // loop through markers
-      for (var i = 0; i < foodMarkers.length; i++){
-        // add marker
-        addMarker(foodMarkers[i]);
-      }
-
-
-     // loop through markers
-     for (var i = 0; i < parkMarkers.length; i++){
-       // add marker
-       addMarker(parkMarkers[i]);
-     }
-
-     // loop through markers
-     for (var i = 0; i < arcadeMarkers.length; i++){
-       // add marker
-       addMarker(arcadeMarkers[i]);
-     }
-
-     // loop through markers
-     for (var i = 0; i < movieMarkers.length; i++){
-       // add marker
-       addMarker(movieMarkers[i]);
-     }
-
-    // add marker function
-    function addMarker (location){
-    var marker = new google.maps.Marker({
-      position: location.latlong,
-      icon: location.iconImg,
-      title: location.locTitle,
-      map: map,
-      animation: google.maps.Animation.DROP
-    });
-
-    // info window
-    var infowindow = new google.maps.InfoWindow({
-      content:location.locTitle
-    });
-
-    // marker event listener to add info window
-    marker.addListener("click", function() {
-      infowindow.open(map, marker);
-    });
-
+  // map presets
+  var mapOptions = {
+    zoom: 11,
+    center: {lat: 29.4241226, lng: -98.493629}
   }
+  // make map
+  map = new
+  google.maps.Map(document.getElementById("map"), mapOptions);
+
+  ko.applyBindings(new ViewModel());
+
+}
+
+// add marker function
+var addMarker  = function(data) {
+  var self = this;
+
+  this.position = data.latlong,
+  this.icon = data.iconImg,
+  this.title = data.locTitle,
+
+  this.marker = new google.maps.Marker({
+    position: this.position,
+    icon: this.icon,
+    title: this.title,
+    map: map,
+    animation: google.maps.Animation.DROP
+  });
+
+  // info window
+  var infowindow = new google.maps.InfoWindow({
+    content:this.title
+  });
+
+  // marker event listener to add info window
+  this.marker.addListener("click", function() {
+    infowindow.open(map, this);
+  });
+}
+
+var addList = function (data) {
+  this.title = ko.observerable(data.locTitle);
+}
+
+var ViewModel = function () {
+  var self = this;
+
+  // create array for sidebar list
+  this.dataList = ko.observableArray([]);
+
+  // loop through markers
+  for (var i = 0; i < data.length; i++){
+    // add marker
+    addMarker(data[i]);
+  }
+
+  // loop through locations
+  for (var i = 0; i < data.length; i++){
+    // add to sidebar
+    this.dataList.push(data[i])
+  }
+
+
+
 }
